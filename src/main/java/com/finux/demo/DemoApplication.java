b613080c51725c9c9a8b0acb83f60ec2bc630900
feigd <feigd@glodon.com>
2020-11-14 10:07:03 +0800

    提交所有的修改

--- src/main/java/com/finux/demo/DemoApplication.java
+++ src/main/java/com/finux/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("------------");
+        System.out.println("===============");
+        System.out.println("+++++++++++++++++");
         SpringApplication.run(DemoApplication.class, args);
     }
 
