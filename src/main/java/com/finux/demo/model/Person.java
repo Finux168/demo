94fe7f07bd7d26398709b691b1ef684ce6852d81
Finux <331665286@qq.com>
2020-11-22 22:51:40 +0800

    增加person类

---
+++ src/main/java/com/finux/demo/model/Person.java
@@ -0,0 +1,80 @@
+package com.finux.demo.model;
+
+/**
+ * 人员类
+ *
+ * @Author: fgd
+ * @Date: 2020/11/22
+ */
+public class Person {
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Integer age;
+
+    private Double weight;
+
+    private Double height;
+
+    private String desc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
